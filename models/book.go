baa760bb4f738051f1cdfa70dd35de7a11bae528
Nedson202 <simeonsamson4@gmail..com>
2019-04-26 11:44:16 +0100

    Add book model

---
+++ models/book.go
@@ -0,0 +1,12 @@
+package models
+
+// BookSchema model
+type BookSchema struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+// Book slice
+var Book = []BookSchema{}
