99538eabb5fbf28143bab2d10db1510cfafb6be2
Nedson202 <simeonsamson4@gmail..com>
2019-04-27 12:19:12 +0100

    Add dynamic routing system

---
+++ routes/routes.go
@@ -0,0 +1,69 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"database/sql"
+
+	"github.com/nedson202/book-api-go/config"
+	"github.com/nedson202/book-api-go/controllers"
+)
+
+var controller = controllers.Controller{}
+
+func getRoutes(db *sql.DB) Routes {
+	var routes = Routes{
+		Route{
+			"GetBooks",
+			"GET",
+			"/books",
+			controller.GetBooks(db),
+		},
+		Route{
+			"GetBook",
+			"GET",
+			"/books/{id}",
+			controller.GetBook(db),
+		},
+		Route{
+			"AddBook",
+			"POST",
+			"/books",
+			controller.AddBook(db),
+		},
+		Route{
+			"UpdateBook",
+			"PUT",
+			"/books/{id}",
+			controller.UpdateBook(db),
+		},
+		Route{
+			"DeleteBook",
+			"DELETE",
+			"/books{id}",
+			controller.RemoveBook(db),
+		},
+	}
+
+	return routes
+}
+
+//NewRouter configures a new router to the API
+func NewRouter(db *sql.DB) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	routes := getRoutes(db)
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = config.Logger(handler, route.Name)
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
---
+++ routes/types.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"net/http"
+)
+
+// Route defines a route
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+//Routes defines the list of routes of our API
+type Routes []Route
